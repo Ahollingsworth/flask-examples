963a133b60b2576dbf5d2f9bbb92a29ff6e27ee1
Cory Forsyth <cory.forsyth@gmail.com>
2014-06-24 23:00:18 -0400

    update install

--- templates/install.sh
+++ templates/install.sh
@@ -7,9 +7,10 @@ INDEX_URL="https://raw.githubusercontent.com/NYC-DOE/flask-examples/master/$APP_
 cd ~
 mkdir -p $APP_NAME
 cd $APP_NAME
-[ -f ./app.py ] && rm ./app.py
+[ -f app.py ] && rm app.py
+curl $APP_URL > app.py
 mkdir -p templates
-[ -f ./templates/index.html ] && rm -rf ./templates/index.html
+[ -f templates/index.html ] && rm -rf templates/index.html
 curl -s $INDEX_URL > templates/index.html
 
 echo "To run this app, type: 'cd && python $APP_NAME/app.py'"
