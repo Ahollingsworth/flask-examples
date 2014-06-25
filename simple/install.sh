a2188c19dbe420b0db132b76e0c3abbee4748689
Cory Forsyth <cory.forsyth@gmail.com>
2014-06-24 22:34:38 -0400

    Update install.sh

--- simple/install.sh
+++ simple/install.sh
@@ -5,6 +5,6 @@ APP_URL=https://raw.githubusercontent.com/NYC-DOE/flask-examples/master/simple/a
 cd ~
 mkdir simple
 cd simple
-curl -s $APP_URL > app.py
+curl -s $APP_URL >> app.py
 
 echo "To run this app, type: 'python simple/app.py'"
