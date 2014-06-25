421c4c5622aff82c2b83a79f153b7cc6cb76e306
Cory Forsyth <cory.forsyth@gmail.com>
2014-06-24 22:41:15 -0400

    update

--- simple/app.py
+++ simple/app.py
@@ -7,4 +7,5 @@ def hello():
 
 if __name__ == "__main__":
     print("To view your site, click the 'preview' menu and choose port 3000")
+    print("To stop your app and return to the command line, type control-C")
     app.run(host='0.0.0.0', port=3000)
