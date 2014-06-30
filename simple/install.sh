4c1b9d2a99fc304feafab0f152748c5ae4621c81
Cory Forsyth <cory.forsyth@gmail.com>
2014-06-29 21:06:15 -0400

    Update install.sh

--- simple/install.sh
+++ simple/install.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-APP_URL=https://raw.githubusercontent.com/NYC-DOE/flask-examples/master/simple/
+APP_URL=https://raw.githubusercontent.com/NYC-DOE/flask-examples/master/simple
 APP_NAME=simple
 
 cd ~
 mkdir -p $APP_NAME
 cd $APP_NAME
 [ -f ./$APP_NAME.py ] && rm ./$APP_NAME.py
+echo "URL"
+echo $APP_URL/$APP_NAME.py
 curl -s $APP_URL/$APP_NAME.py > $APP_NAME.py
 
 echo "To run this app, type: 'cd && python $APP_NAME/$APP_NAME.py'"
