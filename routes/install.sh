f54a0b6d734fc8c305b687b8dbab54db8efe8642
Cory Forsyth <cory.forsyth@gmail.com>
2014-06-29 21:30:48 -0400

    Update install.sh

--- routes/install.sh
+++ routes/install.sh
@@ -7,6 +7,6 @@ cd ~
 mkdir -p $APP_NAME
 cd $APP_NAME
 [ -f ./$APP_NAME.py ] && rm ./$APP_NAME.py
-curl -s $APP_URL > $APP_NAME.py
+curl -s $APP_URL/$APP_NAME.py > $APP_NAME.py
 
 echo "To run this app, type: 'cd && python $APP_NAME/$APP_NAME.py'"
